8fb0e333db1911243ea02f7176adb3ff82fc103e
Alymbekov <maxim.makarov.1997@mail.ru>
2020-04-06 21:16:01 +0600

    add Create, Update, Delete view (crud system)

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import (
+    CreateView, 
+    UpdateView,
+    DeleteView,
+    )
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -20,3 +25,13 @@ class BlogPostCreateView(CreateView):
     fields = '__all__'
 
 
+class BlogPostUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/blog_post_edit.html'
+    fields = ('title', 'body')
+
+
+class BlogPostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/blog_post_delete.html'
+    success_url = reverse_lazy('index')
